4f758b80a0f80c3c31d85bd139eda912dda8b7d8
lorenzowind <lorenzomart01@gmail.com>
2020-08-03 21:57:23 -0400

    feat: navigation configuration and interface header component

---
+++ frontend/src/pages/TeacherList/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
+
+function TeacherList() {
+  return (
+    <div id="page-teacher-list">
+      <PageHeader title="Estes sãos os proffys disponíveis." />
+    </div>
+  )
+}
+
+export default TeacherList;
