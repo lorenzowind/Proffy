a1bcd6fd31dc9921ba948348c1c60cfcddd21d7d
lorenzowind <lorenzomart01@gmail.com>
2020-08-05 16:15:39 -0400

    feat: input component and teacher form styling

--- frontend/src/pages/TeacherList/index.tsx
+++ frontend/src/pages/TeacherList/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
 
 import './styles.css';
 
@@ -10,20 +11,11 @@ function TeacherList() {
     <div id="page-teacher-list">
       <PageHeader title="Estes sãos os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject"/>
-          </div>
+          <Input name="subject" label="Matéria" />
           
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="week_day"/>
-          </div>
+          <Input name="week_day" label="Dia da semana" />
           
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time"/>
-          </div>
+          <Input type="time" name="time" label="Hora" />
         </form>
       </PageHeader>
 
