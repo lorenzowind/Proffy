525fef27f55c5c059851a81171d4523387171c37
lorenzowind <lorenzomart01@gmail.com>
2020-08-05 16:21:34 -0400

    feat: textarea component

--- frontend/src/pages/TeacherForm/index.tsx
+++ frontend/src/pages/TeacherForm/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
 
 import warningIcon from '../../assets/icons/warning.svg';
 
@@ -24,6 +25,8 @@ function TeacherForm() {
           <Input name="avatar" label="Avatar" />
           
           <Input name="whatsapp" label="WhatsApp" />
+        
+          <Textarea name="bio" label="Biografia" />
         </fieldset>
 
         <fieldset>
