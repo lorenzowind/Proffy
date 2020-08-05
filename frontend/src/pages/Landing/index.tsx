f159d46ea46d67db36bca9163dc1092de3258665
lorenzowind <lorenzomart01@gmail.com>
2020-08-05 17:03:16 -0400

    feat: api configuration and total connections request

--- frontend/src/pages/Landing/index.tsx
+++ frontend/src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 import landingImg from '../../assets/landing.svg';
@@ -11,6 +13,14 @@ import purpleHeartIcon from '../../assets/icons/purple-heart.svg';
 import './styles.css';
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      setTotalConnections(response.data.total);
+    })
+  }, []);
+
   return (
   <div id="page-landing">
     <div id="page-landing-content">
@@ -37,7 +47,7 @@ function Landing() {
       </div>
 
       <span className="total-connections">
-        Total de 200 conexões já realizadas.
+        Total de {totalConnections} conexões já realizadas.
         <img src={purpleHeartIcon} alt="Coração roxo"/>
       </span>
     </div>
