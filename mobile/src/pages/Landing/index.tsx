b94cd04df59dc349beeb6394e1b3fde7d0c9a1f0
lorenzowind <lorenzomart01@gmail.com>
2020-08-06 18:23:07 -0400

    feat: tab routes navigation configuration

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -16,6 +16,10 @@ function Landing() {
   function handleNavigateToGiveClassesPage() {
     navigate('GiveClasses');
   }
+
+  function handleNavigateToStudyPages() {
+    navigate('Study')
+  }
   
   return (
     <View style={styles.container}>
@@ -30,7 +34,10 @@ function Landing() {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <RectButton style={[styles.button, styles.buttonPrimary]}>
+        <RectButton 
+          onPress={handleNavigateToStudyPages}
+          style={[styles.button, styles.buttonPrimary]}
+        >
           <Image source={studyIcon} />
 
           <Text style={styles.buttonText}>Estudar</Text>
