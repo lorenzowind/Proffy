953d189e1b3a3080481005e4b3a6b26ec6304ee0
lorenzowind <lorenzomart01@gmail.com>
2020-08-06 17:37:01 -0400

    feat: landing page styling

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
 import landingImg from '../../assets/landing.png';
+import studyIcon from '../../assets/icons/study.png';
+import giveClassesIcon from '../../assets/icons/give-classes.png';
+import heartIcon from '../../assets/icons/heart.png';
 
 function Landing() {
   return (
@@ -16,6 +19,25 @@ function Landing() {
         <Text style={styles.titleBold}>
           O que deseja fazer?
         </Text>
+      </Text>
+
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+          <Image source={studyIcon} />
+
+          <Text style={styles.buttonText}>Estudar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+          <Image source={giveClassesIcon} />
+
+          <Text style={styles.buttonText}>Dar aulas</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={styles.totalConnections}>
+        Total de 200 conexões já realizadas {' '}
+        <Image source={heartIcon} />
       </Text>
     </View>
   );
