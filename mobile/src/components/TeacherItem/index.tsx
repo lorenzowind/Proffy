ea0d2ad4fef0eeb19c3222032e839d2093acb335
lorenzowind <lorenzomart01@gmail.com>
2020-08-07 23:39:43 -0400

    feat: connection creating and favorites listing

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -3,12 +3,13 @@ import { View, Image, Text, Linking } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import AsyncStorage from '@react-native-community/async-storage';
 
+import api from '../../services/api';
+
 import heartOutlineIcon from '../../assets/icons/heart-outline.png';
 import unfavoriteIcon from '../../assets/icons/unfavorite.png';
 import whatsappIcon from '../../assets/icons/whatsapp.png';
 
 import styles from './styles';
-
 export interface Teacher {
   id: number;
   avatar: string;
@@ -28,6 +29,10 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
   const [isFavorited, setIsFavorited] = useState(favorited);
 
   function handleLinkToWhatsapp() {
+    api.post('connections', {
+      user_id: teacher.id,
+    });
+
     Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
   }
 
